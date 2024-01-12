e0208fda4efc9edad308867fbad6fbb79f964491
Alexander Fisunov <fisunov.a.v@gmail.com>
2024-01-12 22:07:31 +0300

    фикс аргументов

--- src/main/java/ru/fisunov/java/basic/homeworks/homework1/MainApplication.java
+++ src/main/java/ru/fisunov/java/basic/homeworks/homework1/MainApplication.java
@@ -2,10 +2,10 @@ package ru.fisunov.java.basic.homeworks.homework1;
 
 public class MainApplication {
     public static void main(String[] args) {
-        sumAndPrint(1, 1);
+        sumAndPrint(1, 1, 2);
     }
 
-    public static void sumAndPrint(int a, int b) {
-        System.out.println(a + b);
+    public static void sumAndPrint(int a, int b, int c) {
+        System.out.println(a + b + c);
     }
 }
