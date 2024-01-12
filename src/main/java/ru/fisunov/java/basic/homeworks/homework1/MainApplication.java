7b6221c0160524c7dbcec7a948b8145bb106e298
Alexander Fisunov <fisunov.a.v@gmail.com>
2024-01-12 22:05:45 +0300

    ДЗ 1 выполнено

---
+++ src/main/java/ru/fisunov/java/basic/homeworks/homework1/MainApplication.java
@@ -0,0 +1,11 @@
+package ru.fisunov.java.basic.homeworks.homework1;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        sumAndPrint(1, 1);
+    }
+
+    public static void sumAndPrint(int a, int b) {
+        System.out.println(a + b);
+    }
+}
